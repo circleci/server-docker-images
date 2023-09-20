7543cecd6e2312451de133ad4b5c88944220225b
Ryan William O'Hara <ryanwohara@users.noreply.github.com>
2023-09-20 07:56:58 -0600

    fix: Allow cron for WFC
    Workflows-conductor needs conductor_production to have cron support.

--- postgresql/12/rootfs/docker-entrypoint-initdb.d/00-init-pg-cron.sh
+++ postgresql/12/rootfs/docker-entrypoint-initdb.d/00-init-pg-cron.sh
@@ -2,4 +2,4 @@
 
 # N.b., any global environment variables are defined in `/opt/bitnami/postgresql-env.sh`
 echo "shared_preload_libraries = 'pg_cron'" >>"${POSTGRESQL_CONF_FILE}"
-echo "cron.database_name = '${POSTGRESQL_DATABASE:-}'" >>"${POSTGRESQL_CONF_FILE}"
+echo "cron.database_name = 'conductor_production'" >>"${POSTGRESQL_CONF_FILE}" # this is used by workflows-conductor
